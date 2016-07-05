ef842f5be520054a12eb9037994f53ddcc185166
Alex Steele <steeleal123@gmail.com>
2016-07-04 18:02:32 -0600

    Fix key listener to allow holding down of modifier keys. Allow native keydown actions.

--- app/keys.js
+++ app/keys.js
@@ -2,20 +2,22 @@
 
 const defaultKeyMap = (function() {
     const keys = {
-        'Enter':      {type: 'INSERT_NEW_LINE'},
-        'Tab':        {type: 'INSERT', text: '    '}, // TODO: Change this to reflect preferences/mode etc.
-        'Backspace':  {type: 'DELETE_BACK_CHAR'},
-        'Control-k':  {type: 'KILL_LINE'},
-        'ArrowLeft':  {type: 'MOVE_CURSOR_LEFT'},
-        'Control-b':  {type: 'MOVE_CURSOR_LEFT'},
-        'ArrowRight': {type: 'MOVE_CURSOR_RIGHT'},
-        'Control-f':  {type: 'MOVE_CURSOR_RIGHT'},
-        'ArrowUp':    {type: 'MOVE_CURSOR_UP'},
-        'Control-p':  {type: 'MOVE_CURSOR_UP'},
-        'ArrowDown':  {type: 'MOVE_CURSOR_DOWN'},
-        'Control-n':  {type: 'MOVE_CURSOR_DOWN'},
-        'Control-a':  {type: 'MOVE_CURSOR_BEGINNING_OF_LINE'},
-        'Control-e':  {type: 'MOVE_CURSOR_END_OF_LINE'}
+        'Enter':         {type: 'INSERT_NEW_LINE'},
+        'Tab':           {type: 'INSERT', text: '    '}, // TODO: Change this to reflect preferences/mode etc.
+        'Backspace':     {type: 'DELETE_BACK_CHAR'},
+        'Control-k':     {type: 'KILL_LINE'},
+        'ArrowLeft':     {type: 'MOVE_CURSOR_LEFT'},
+        'Control-b':     {type: 'MOVE_CURSOR_LEFT'},
+        'ArrowRight':    {type: 'MOVE_CURSOR_RIGHT'},
+        'Control-f':     {type: 'MOVE_CURSOR_RIGHT'},
+        'ArrowUp':       {type: 'MOVE_CURSOR_UP'},
+        'Control-p':     {type: 'MOVE_CURSOR_UP'},
+        'ArrowDown':     {type: 'MOVE_CURSOR_DOWN'},
+        'Control-n':     {type: 'MOVE_CURSOR_DOWN'},
+        'Control-a':     {type: 'MOVE_CURSOR_BEGINNING_OF_LINE'},
+        'Control-e':     {type: 'MOVE_CURSOR_END_OF_LINE'},
+        'Alt-Meta-Dead': {type: 'NATIVE!'},
+        'Meta-q':        {type: 'NATIVE!'}
     };
     
     ['a', 'b', 'c', 'd',
@@ -51,8 +53,6 @@ function createKeyListener(elem, keyMap, onKeyAction, onKeyError) {
     const activeModifiers = [];
     
     elem.addEventListener('keydown', (e) => {
-        e.preventDefault();
-
         if (keyIsModifier(e.key)) {
             activeModifiers.push(e.key);
             return;
@@ -65,12 +65,15 @@ function createKeyListener(elem, keyMap, onKeyAction, onKeyError) {
         const action = keyMap[withModifiers];
 
         if (action) {
+            if (action.type === 'NATIVE!') {
+                return;
+            }
             onKeyAction(action);
         } else {
             onKeyError(withModifiers);
         }
 
-        activeModifiers.splice(0, activeModifiers.length); 
+         e.preventDefault();
     });
 
     elem.addEventListener('keypress', (e) => {
@@ -79,7 +82,7 @@ function createKeyListener(elem, keyMap, onKeyAction, onKeyError) {
 
     elem.addEventListener('keyup', (e) => {
         e.preventDefault();
-        
+
         const index = activeModifiers.indexOf(e.key);
         if (index !== -1) {
             activeModifiers.splice(index, 1);
