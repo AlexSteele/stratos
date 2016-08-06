10806d3d3a018438e2d7c3bd8de299dff520a5cb
Alex Steele <steeleal123@gmail.com>
2016-08-06 15:23:29 -0600

    Add lineRange.js

---
+++ app/lineRange.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function LineRange() {
+    this.startLine = -1;
+    this.startCol = -1;
+    this.endLine = -1;
+    this.endCol = -1;
+};
+
+LineRange.prototype.setTo = function(startLine, startCol, endLine, endCol) {
+    this.startLine = startLine;
+    this.startCol = startCol;
+    this.endLine = endLine;
+    this.endCol = endCol;
+};
+
+LineRange.prototype.clear = function() {
+    this.startLine = -1;
+    this.startCol = -1;
+    this.endLine = -1;
+    this.endCol = -1;
+};
+
+LineRange.prototype.isEmpty = function() {
+    return this.startLine === -1 &&
+        this.startCol === -1 &&
+        this.endLine === -1 &&
+        this.endCol === -1;
+};
+
+LineRange.prototype.splat = function() {
+    return {
+        startLine: this.startLine,
+        startCol: this.startCol,
+        endLine: this.endLine,
+        endCol: this.endCol
+    };
+};
+
+module.exports = LineRange;
